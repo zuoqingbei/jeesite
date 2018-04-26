6ce6a8440f013e3dda96ef7515ca0e306ab95146
tangmumu <2679167174@qq.com>
2018-04-26 18:44:50 +0800

    总况产品交易额占比

---
+++ src/main/java/com/qdch/portal/littleproject/entity/ProductClassifyModel.java
@@ -0,0 +1,7 @@
+package com.qdch.portal.littleproject.entity;
+
+import com.qdch.portal.common.persistence.DataEntity;
+
+public class ProductClassifyModel extends DataEntity<ProductClassifyModel>{
+
+}
